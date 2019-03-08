9b0838fe42bd0d9d975a4dc8f08e46bfb5fa91d2
decongo <conger.devon@gmail.com>
2019-03-08 15:48:26 -0700

    stuff

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isSelected: false,
-        times: []
+        days: []
     },
     computed: {
 
@@ -11,15 +10,39 @@ var app = new Vue({
         this.createTable();
     },
     methods: {
-        toggleSelected() {
-            this.isSelected = !this.isSelected
+        toggleSelected(time) {
+            time.isSelected = !time.isSelected
         },
         createTable() {
-            for (let i = 6; i < 21; i++) {
-                this.times.push({
-                    hour: i
-                });
+            for (let i = 0; i < 7; ++i) {
+                let newTimes = [];
+                for (let j = 6; j < 21; j++) {
+                    newTimes.push({
+                        hour: j,
+                        isSelected: false
+                    });
+                }
+                this.days.push({
+                    times: newTimes
+                })
+
             }
         },
     }
 })
+
+document.addEventListener("dragenter", function (event) {
+    // highlight potential drop target when the draggable element enters it
+    if (event.target.className == "time-card") {
+        app.toggleSelected();
+    }
+
+}, false);
+
+document.addEventListener("dragleave", function (event) {
+    // reset background of potential drop target when the draggable element leaves it
+    if (event.target.className == "time-card") {
+        event.target.style.background = "";
+    }
+
+}, false);
