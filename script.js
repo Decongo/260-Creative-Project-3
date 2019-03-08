56cb44255502012761198f309e260a6cb4cad663
decongo <conger.devon@gmail.com>
2019-03-08 14:33:22 -0700

    started table

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        isSelected: false,
+        times: []
     },
     computed: {
 
     },
+    created: function () {
+        this.createTable();
+    },
     methods: {
-
+        toggleSelected() {
+            this.isSelected = !this.isSelected
+        },
+        createTable() {
+            for (let i = 6; i < 21; i++) {
+                this.times.push({
+                    hour: i
+                });
+            }
+        },
     }
 })
