c079879a8ae10084db404e53404032e73557db7a
kaden-weber <kweber142@gmail.com>
2019-03-08 16:19:45 -0700

    Mostly ready

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        days: []
-    },
-    computed: {
+  el: '#app',
+  data: {
+    days: []
+  },
+  computed: {
 
+  },
+  created: function() {
+    this.createTable();
+  },
+  methods: {
+    toggleSelected(time) {
+      time.isSelected = !time.isSelected;
     },
-    created: function () {
-        this.createTable();
+    toggleDay() {
+      for (time in this.days[0].times) {
+        this.toggleSelected(time);
+      }
     },
-    methods: {
-        toggleSelected(time) {
-            time.isSelected = !time.isSelected
-        },
-        createTable() {
-            for (let i = 0; i < 7; ++i) {
-                let newTimes = [];
-                for (let j = 6; j < 21; j++) {
-                    newTimes.push({
-                        hour: j,
-                        isSelected: false
-                    });
-                }
-                this.days.push({
-                    times: newTimes
-                })
+    createTable() {
+      for (let i = 0; i < 7; ++i) {
+        let newTimes = [];
+        for (let j = 6; j < 22; j++) {
+          newTimes.push({
+            hour: j,
+            isSelected: false
+          });
+        }
+        this.days.push({
+          times: newTimes
+        })
 
-            }
-        },
-    }
+      }
+    },
+  }
 })
-
-document.addEventListener("dragenter", function (event) {
-    // highlight potential drop target when the draggable element enters it
-    if (event.target.className == "time-card") {
-        app.toggleSelected();
-    }
-
-}, false);
-
-document.addEventListener("dragleave", function (event) {
-    // reset background of potential drop target when the draggable element leaves it
-    if (event.target.className == "time-card") {
-        event.target.style.background = "";
-    }
-
-}, false);
